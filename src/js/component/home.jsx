2e934cf9e9bdbb8622191868c24d2133ecff0ed5
MadamSagas <141463441+MadamSagas@users.noreply.github.com>
2024-04-15 17:03:41 +0000

    agregue el codigo js en el archivo home.jsx tambien los estilos al index.css, agregue el link de font awesome al template.html

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  const [inputValue, setInputValue] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  return (
+    <div className="container">
+      <h1 className="mb-3 text-primary">My Todos</h1>
+      <ul>
+        <li className="m-3 p-3">
+          
+          <input
+          className="form-control mb-3"
+            type="text"
+            onChange={(e) => setInputValue(e.target.value)}
+            value={inputValue}
+            onKeyDown={(e) => {
+              if (e.key === "Enter") {
+                setTodos(todos.concat([inputValue]));
+                setInputValue("");
+              }
+            }}
+            placeholder="What do you need to do"
+            
+          />
+          {todos.map((item, index) => (
+            <li className="fs-4 text list-group-item m-3 py-1 px-3">
+              {item}{" "}
+              <i
+                class="fa-solid fa-square-xmark ms-5 fs-4 text text-primary btn-delete"
+                onClick={() =>
+                  setTodos(
+                    todos.filter((t, currentIndex) => index != currentIndex)
+                  )
+                }
+              ></i>
+            </li>
+          ))}
+        </li>
+      </ul>
+      <div>
+        {todos.length > 0 ? (
+          <p className="mt-3 m-3 py-1 px-3">Cantidad de tareas: {todos.length}</p>
+        ) : (
+          <p className="mt-3 ms-4 py-1 px-3">No hay tareas por hacer</p>
+        )
+        }
+      </div>
+
+      
+    </div>
+  );
 };
 
 export default Home;
